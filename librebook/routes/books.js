822d6f17f812020d5cf00f464adc78d9df79939b
lukasz mistura <lukasz.mistura@contour-technology.com>
2017-03-20 01:09:47 +0100

    Fix books/add

--- librebook/routes/books.js
+++ librebook/routes/books.js
@@ -42,70 +42,13 @@ router.route('/')
             }
         });
     })
-// route middleware to validate :id
-router.param('id', function(req, res, next, id) {
-    //console.log('validating ' + id + ' exists');
-    //find the ID in the Database
-    mongoose.model('Book').findById(id, function (err, book) {
-        //if it isn't found, we are going to repond with 404
-        if (err) {
-            console.log(id + ' was not found');
-            res.status(404)
-            var err = new Error('Not Found');
-            err.status = 404;
-            res.format({
-                html: function(){
-                    next(err);
-                 },
-                json: function(){
-                       res.json({message : err.status  + ' ' + err});
-                 }
-            });
-        //if it is found we continue on
-        } else {
-
-            console.log('ELSE');
-
-            //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
-            //console.log(book);
-            // once validation is done save the new item in the req
-            req.id = id;
-            // go to the next thing
-            next();
-        }
-    });
-});
-//POST a new book
-router.route('/:id')
-    .get(function (req, res) {
-        console.log("req.id:", req.id);
-        mongoose.model('Book').findById(req.id, function (err, book) {
-            console.log("book._id:", book._id);
-
-            if(err){
-                console.log('There was a problem retrieving book ' + book._id);
-            } else {
-                console.log("GET retrieving ID:" + book._id);
-                res.format({
-                    html: function() {
-                        res.render('books/show', {
-                            'book': book
-                        })
-                    },
-                    json: function(){
-                        res.json(book)
-                    }
-                })
-            }
-
-            console.log("book:", book);
-        })
-
-    })
 
 router.route('/new')
     .get(function (req, res) {
         // res.send('about page')
+        console.log('NEW');
+
+
         res.render('books/new', { title: 'Add new book' })
     })
     .post(function (req, res) {
@@ -140,5 +83,156 @@ router.route('/new')
             }
         })
     });
+
+// route middleware to validate :id
+router.param('id', function (req, res, next, id) {
+    //console.log('validating ' + id + ' exists');
+    //find the ID in the Database
+    console.log("id:", id);
+
+    mongoose.model('Book').findById(id, function (err, book) {
+        //if it isn't found, we are going to repond with 404
+        if(err) {
+            console.log(id + ' was not found');
+            res.status(404)
+            var err = new Error('Not Found');
+            err.status = 404;
+            res.format({
+                html: function () {
+                    next(err);
+                },
+                json: function () {
+                    res.json({ message: err.status + ' ' + err });
+                }
+            });
+            //if it is found we continue on
+        } else {
+            //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
+            //console.log(book);
+            // once validation is done save the new item in the req
+            console.log('VALIDATED');
+
+            req.id = id;
+            // go to the next thing
+            next();
+        }
+    });
+});
+
+//POST a new book
+router.route('/:id')
+    .get(function (req, res) {
+        mongoose.model('Book').findById(req.id, function (err, book) {
+            if(err) {
+                console.log('There was a problem retrieving book ' + book._id);
+            } else {
+                if(!book) {
+                    console.log('Empty bookstore!');
+                } else {
+                    console.log("GET retrieving ID:" + book._id);
+                    res.format({
+                        html: function () {
+                            res.render('books/show', {
+                                'book': book
+                            })
+                        },
+                        json: function () {
+                            res.json(book)
+                        }
+                    })
+                }
+            }
+        })
+
+    })
+
+//GET the individual book by Mongo ID
+router.get('/:id/edit', function (req, res) {
+    //search for the book within Mongo
+    console.log('GET');
+
+    mongoose.model('Book').findById(req.id, function (err, book) {
+        if(err) {
+            console.log('GET Error: There was a problem retrieving: ' + err);
+        } else {
+            //Return the book
+            console.log('GET Retrieving ID: ' + book._id);
+            //format the date properly for the value to show correctly in our edit form
+            res.format({
+                //HTML response will render the 'edit.jade' template
+                html: function () {
+                    res.render('books/edit', {
+                        title: 'Book' + book._id,
+                        qty: book.qty
+                    });
+                },
+                //JSON response will return the JSON output
+                json: function () {
+                    res.json(book);
+                }
+            });
+        }
+    });
+});
+
+router.put('/:id/edit', function (req, res) {
+    mongoose.model('Book').findById(req.id, function (err, book) {
+        book.update({
+            title: req.body.title,
+            qty: req.body.qty
+        }, function (err, bookID) {
+            if(err) {
+                res.send("There was a problem updating the information to the database: " + err);
+            } else {
+                //HTML responds by going back to the page or you can be fancy and create a new view that shows a success page.
+                res.format({
+                    html: function () {
+                        res.redirect("/books/" + book._id);
+                    },
+                    //JSON responds showing the updated values
+                    json: function () {
+                        res.json(book);
+                    }
+                });
+            }
+        })
+    })
+})
+
+router.delete('/:id/edit', function (req, res) {
+    //find book by ID
+    mongoose.model('Book').findById(req.id, function (err, book) {
+        if(err) {
+            return console.error(err);
+        } else {
+            //remove it from Mongo
+            console.log('DELETE');
+
+
+            book.remove(function (err, book) {
+                if(err) {
+                    return console.error(err);
+                } else {
+                    //Returning success messages saying it was deleted
+                    console.log('DELETE removing ID: ' + book._id);
+                    res.format({
+                        //HTML returns us back to the main page, or you can create a success page
+                        html: function () {
+                            res.redirect("/books");
+                        },
+                        //JSON returns the item with the message that is has been deleted
+                        json: function () {
+                            res.json({
+                                message: 'deleted',
+                                item: book
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
 
 module.exports = router;
