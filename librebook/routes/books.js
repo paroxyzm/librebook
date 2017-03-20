460e9e64ff4af5eb6bb3c1f3d78b318eb4df73ba
lukasz mistura <lukasz.mistura@contour-technology.com>
2017-03-20 01:20:30 +0100

    VERY simple crud

--- librebook/routes/books.js
+++ librebook/routes/books.js
@@ -10,7 +10,7 @@ var methodOverride = require('method-override');
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(methodOverride(function (req, res) {
     if(req.body && typeof req.body === 'object' && '_method' in req.body) {
-        var method = req.body._mehtod;
+        var method = req.body._method;
         delete req.body._method;
         return method;
     }
